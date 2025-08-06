49e476b0c81ba23c761a8cd8b4970bc666042d68
Vivian <vivian@limesli.me>
2025-08-06 17:14:22 -0400

    Change boss defeats to be based on room clear instead of enemy kill

--- locations/enemy_destruction.lua
+++ locations/enemy_destruction.lua
@@ -1,82 +1,3 @@
--- Never check these entities. They will be subject to the "seen" method of defeating
-local excludedEntities = {
-    -- Larry Jr.
-    {type = 19, variant = 0},
-
-    -- The Hollow
-    {type = 19, variant = 1},
-
-    -- Tuff Twin
-    {type = 19, variant = 2},
-
-    -- The Shell
-    {type = 19, variant = 3},
-
-    -- Brownie
-    {type = 402, variant = 0},
-
-    -- Gurgling
-    {type = 237, variant = 1},
-
-    -- Turdling
-    {type = 237, variant = 2},
-
-    -- The Frail
-    {type = 62, variant = 2},
-
-    -- Gemini (Main)
-    {type = 79, variant = 0},
-
-    -- Steven (Main)
-    {type = 79, variant = 1},
-
-    -- Headless Horseman (Body)
-    {type = 82, variant = 0},
-
-    -- Sister Vis
-    {type = 410, variant = 0},
-
-    -- Fistula, Teratoma
-    {type = 71, variant = 0},
-    {type = 71, variant = 1},
-    {type = 72, variant = 0},
-    {type = 72, variant = 1},
-    {type = 73, variant = 0},
-    {type = 73, variant = 1},
-
-    -- Blastocyst
-    {type = 74, variant = 0},
-    {type = 75, variant = 0},
-    {type = 76, variant = 0},
-
-    -- Envy
-    {type = 51, variant = 0},
-    {type = 51, variant = 10},
-    {type = 51, variant = 20},
-    {type = 51, variant = 30},
-
-    -- Super Envy
-    {type = 51, variant = 1},
-    {type = 51, variant = 11},
-    {type = 51, variant = 21},
-    {type = 51, variant = 31},
-
-    -- The Fallen
-    {type = 81, variant = 0},
-
-    -- The Matriarch
-    {type = 413, variant = 0},
-
-    -- Rotgut
-    {type = 911, variant = 0},
-
-    -- Chimera
-    {type = 910, variant = 0},
-
-    -- Lokii
-    {type = 69, variant = 1}
-}
-
 -- Tries to grant a location check for defeating this entity.
 local function tryGrantLocationForTypeVariant(type, variant)
     local nameKey = tostring(type) .. ":" .. tostring(variant)
@@ -98,22 +19,14 @@ end
 -- Enemies we've seen this room
 local seen = {}
 
--- Handles locations for defeated enemies
+-- Handles "seeing" enemies (that we have defeated!)
 AP_MAIN_MOD:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, function (_, entity)
     local type = entity.Type
     local variant = entity.Variant
 
     -- Mark enemies that we've "seen" (read: killed) in this room.
-    -- When the room is completed, checks for multipart enemies will be granted and this will be flushed
-    seen[#seen] = {type = type, variant = variant}
-
-    for _, v in ipairs(excludedEntities) do
-        if v.type == type and v.variant == variant then
-            return -- Not checking this entity
-        end
-    end
-
-    tryGrantLocationForTypeVariant(type, variant)
+    -- When the room is completed, checks will be granted and this will be flushed
+    seen[#seen + 1] = {type = type, variant = variant}
 end)
 
 AP_MAIN_MOD:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, function ()
