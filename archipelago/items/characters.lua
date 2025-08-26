e4b8929de30fa47f1ff78e8cd5b665358f9f129b
Vivian <vivian@limesli.me>
2025-08-25 22:26:36 -0700

    Greedier mode locking

--- archipelago/items/characters.lua
+++ archipelago/items/characters.lua
@@ -3,17 +3,21 @@ local font = Archipelago.fonts.Terminus
 local runLocked = false
 local characterLocked = false
 local challengeLocked = false
+local greedierLocked = false
 
 --- Locks the run.
 --- @param character boolean If the cause was because the character was locked
 --- @param challenge boolean If the cause was because the challenge was locked
-local function isLocked(character, challenge)
+--- @param greedier boolean If the cause was because greedier was locked
+local function isLocked(character, challenge, greedier)
     runLocked = true
 
     if character then
         characterLocked = true
     elseif challenge then
         challengeLocked = true
+    elseif greedier then
+        greedierLocked = true
     end
 
     -- Add costumes
@@ -30,6 +34,16 @@ local function isLocked(character, challenge)
     for slot, door in util.doors(room) do
         room:RemoveDoor(slot)
     end
+
+    -- Remove the greed mode button
+    for index, gridEntity in util.gridEntities(room) do
+        if gridEntity and gridEntity:GetType() == GridEntityType.GRID_PRESSURE_PLATE and gridEntity:GetVariant() == 2 then
+            room:RemoveGridEntity(index, 0, true)
+            gridEntity:Update()
+        end
+    end
+
+    room:Update()
 end
 
 --- Checks if the currently played character is locked, then applies locked code if it is.
@@ -40,7 +54,7 @@ local function checkCharacterLocked()
     local code = Archipelago.ITEMS_DATA.CHARACTER_ID_TO_CODE[character]
 
     if not Archipelago:checkUnlocked(code) then
-        isLocked(true, false)
+        isLocked(true, false, false)
     else
         characterLocked = false -- In case we had previously determined it was
     end
@@ -59,9 +73,24 @@ local function checkChallengeLocked()
     local challengeName = Archipelago.CHALLENGE_DATA.CHALLENGE_ID_TO_NAME[challengeId]
 
     if not Archipelago:checkUnlockedByName(challengeName) then
-        isLocked(false, true)
+        isLocked(false, true, false)
     else
         challengeLocked = false
+    end
+end
+
+--- Checks if the greedier mode is locked, and we're in it.
+local function checkGreedierLocked()
+    -- Not playing Greedier
+    if Archipelago.game.Difficulty ~= Difficulty.DIFFICULTY_GREEDIER then
+        greedierLocked = false
+        return
+    end
+
+    if not Archipelago:checkUnlocked(Archipelago.ITEMS_DATA.NAME_TO_CODE["Greedier!"]) then
+        isLocked(false, false, true)
+    else
+        greedierLocked = false -- In case we had previously determined it was
     end
 end
 
@@ -73,8 +102,9 @@ Archipelago:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, function (_, continue
 
     checkCharacterLocked()
     checkChallengeLocked()
+    checkGreedierLocked()
 
-    if not characterLocked and not challengeLocked then
+    if not characterLocked and not challengeLocked and not greedierLocked then
         runLocked = false -- Neither is locked, run is free to go
     end
 end)
@@ -94,6 +124,8 @@ Archipelago:AddCallback(ModCallbacks.MC_POST_RENDER, function()
     local text = "This character is locked!"
     if challengeLocked then
         text = "This challenge is locked!"
+    elseif greedierLocked then
+        text = "Greedier Mode is locked!"
     end
 
     local textWidth = font:GetStringWidth(text)
